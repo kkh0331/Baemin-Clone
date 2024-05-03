44ce2a71af9d7b8fde5cefc40e2770aae679ec84
kihyun.kwon <kkh3272@naver.com>
2024-05-03 15:41:21 +0900

    feat: init first spring setting

---
+++ src/main/java/pda/baeminclone/BaeminCloneApplication.java
@@ -0,0 +1,13 @@
+package pda.baeminclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeminCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeminCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pda/baeminclone/BaeminCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package pda.baeminclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeminCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
